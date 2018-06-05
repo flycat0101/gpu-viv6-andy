2e4245f20c9d7ba163d63ac84c6fa6dd302ee920
Xianzhong <xianzhong.li@nxp.com>
2018-06-06 02:42:00 +0800

    6.2.4.p2-0062-IMX-921-BUG-20250-CL156291-NXP-QM-arm-test-case-cl_c
    IMX-921 BUG#20250 CL156291 [NXP][QM] arm test case,
     cl_convolution, met GPU hang CL156291 fix bug 20250,
     [NXP][QM] arm test case, cl_convolution, met GPU hang.
    
    Signed-off-by: Xianzhong <xianzhong.li@nxp.com>

--- driver/hal/kernel/gc_hal_kernel_event.c
+++ driver/hal/kernel/gc_hal_kernel_event.c
@@ -2162,9 +2162,8 @@ gckEVENT_Notify(
 #if gcdUSE_MMU_EXCEPTION
 #if gcdALLOC_ON_FAULT
             status = gckHARDWARE_HandleFault(Event->kernel->hardware);
-
-            if (gcmIS_ERROR(status))
 #endif
+            if (gcmIS_ERROR(status))
             {
                 /* Dump error is fault can't be handle. */
                 gckHARDWARE_DumpMMUException(Event->kernel->hardware);
