4752ba335ac2a640805d8ff6a11ab9944e490e94
Yong Gan <yong.gan@nxp.com>
2018-08-04 08:39:05 +0800

    MGS-3501-1 [#imx-1109] revert the changes for asynchrounous swap in gbm
    Revert the change to commit false.
    Because flicker was found in multiple GL and video player test.
    
    Signed-off-by Yong Gan <yong.gan@ncp.com>

--- driver/driver/khronos/libEGL/source/gc_egl_swap.c
+++ driver/driver/khronos/libEGL/source/gc_egl_swap.c
@@ -2032,15 +2032,7 @@ _SwapBuffersRegion(
         if ((dpy->workerThread == gcvNULL) || synchronous)
         {
             /* Commit-stall. */
-            if(platform->platform == EGL_PLATFORM_GBM_VIV)
-            {
-                /*Use the KMS fence to sync the display buffer*/
-                gcmVERIFY_OK(gcoHAL_Commit(gcvNULL, gcvFALSE));
-            }
-            else
-            {
-                gcmVERIFY_OK(gcoHAL_Commit(gcvNULL, gcvTRUE));
-            }
+            gcmVERIFY_OK(gcoHAL_Commit(gcvNULL, gcvTRUE));
 
             /* Post back buffer. */
             if (!platform->postWindowBackBuffer(dpy, draw,
