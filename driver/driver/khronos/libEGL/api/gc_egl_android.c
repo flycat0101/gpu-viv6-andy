eccb15d8eedde60a0786d5e8af39be6598c6a0d9
Xianzhong <xianzhong.li@nxp.com>
2018-09-08 05:42:54 +0800

    6.2.4.p3-0036-CL167122-egl-update-dma-buf-meta-data-for-fb-buffers
    CL167122 egl: update dma-buf meta data for fb buffers on
     android kms display requires meta data to obtain ts states.
     to obtain ts states. to obtain ts states. to obtain ts
     states. to obtain ts states. This is for issue IMX-848
    
    Signed-off-by: Xianzhong <xianzhong.li@nxp.com>

--- driver/driver/khronos/libEGL/api/gc_egl_android.c
+++ driver/driver/khronos/libEGL/api/gc_egl_android.c
@@ -302,6 +302,7 @@ struct eglWindowInfo
         void *          data;
 
         gcoSURF         surface;
+        int             tsFd;
         EGLint          age;
     } bufferCache[32];
 };
@@ -1743,6 +1744,12 @@ _InvalidateBufferCache(
         {
             /* Unref the surface. */
             gcoSURF_Destroy(info->bufferCache[i].surface);
+
+            if (info->bufferCache[i].tsFd >= 0)
+            {
+                close(info->bufferCache[i].tsFd);
+                info->bufferCache[i].tsFd = -1;
+            }
         }
     }
 
@@ -1759,9 +1766,9 @@ _UpdateBufferCache(
     EGLint i;
     VEGLWindowInfo info = EglSurface->winInfo;
     native_handle_t *handle = (native_handle_t *)Buffer->handle;
-    void * bo = NULL;
     gcoSURF surface;
     int fd;
+    int tsFd = -1;
     void * data;
     EGLBoolean invalidate = EGL_FALSE;
 
@@ -1813,11 +1820,22 @@ _UpdateBufferCache(
         return EGL_FALSE;
     }
 
+#ifdef DRM_GRALLOC
+    /* Get ready for meta date for framebuffer. */
+    if (data && info->hwFramebuffer)
+    {
+        /* Export ts fd for metadata. */
+        gctUINT32 tsNode = surface->tileStatusNode.u.normal.node;
+        gcmVERIFY_OK(gcoHAL_ExportVideoMemory(tsNode, O_RDWR, &tsFd));
+    }
+#endif
+
     /* Not cached yet, cache it. */
     info->bufferCache[i].handle  = handle;
     info->bufferCache[i].fd      = fd;
     info->bufferCache[i].data    = data;
     info->bufferCache[i].surface = surface;
+    info->bufferCache[i].tsFd    = tsFd;
     info->bufferCache[i].age     = 0;
 
     /* This assignment should be atomic to support concurrent. */
@@ -2807,6 +2825,33 @@ _PushBufferStatus(
 }
 #endif
 
+#ifdef DRM_GRALLOC
+static EGLBoolean
+_UpdateBufferMetadata(
+    IN VEGLSurface EglSurface,
+    IN gcoSURF Surface
+    )
+{
+    EGLint i;
+    VEGLWindowInfo info = EglSurface->winInfo;
+
+    for (i = 0; i < info->bufferCacheCount; i++)
+    {
+        if (info->bufferCache[i].surface == Surface)
+        {
+            break;
+        }
+    }
+
+    if (i < info->bufferCacheCount && info->bufferCache[i].tsFd >= 0)
+    {
+        gcoSURF_UpdateMetadata(Surface, info->bufferCache[i].tsFd);
+    }
+
+    return EGL_TRUE;
+}
+#endif
+
 static EGLBoolean
 _PostWindowBackBuffer(
     IN VEGLDisplay Display,
@@ -2830,6 +2875,9 @@ _PostWindowBackBuffer(
 #ifdef DRM_GRALLOC
     /* Update buffer ts info and timestamp. */
     _PushBufferStatus(buffer, BackBuffer->surface);
+
+    /* Update surface metadata. */
+    _UpdateBufferMetadata(Surface, BackBuffer->surface);
 #else
     /* Surface has changed. */
     gcmVERIFY_OK(gcoSURF_UpdateTimeStamp(BackBuffer->surface));
@@ -2872,6 +2920,9 @@ _PostWindowBackBufferFence(
 #ifdef DRM_GRALLOC
     /* Update buffer ts info and timestamp. */
     _PushBufferStatus(buffer, BackBuffer->surface);
+
+    /* Update surface metadata. */
+    _UpdateBufferMetadata(Surface, BackBuffer->surface);
 #else
     /* Surface has changed. */
     gcmVERIFY_OK(gcoSURF_UpdateTimeStamp(BackBuffer->surface));
