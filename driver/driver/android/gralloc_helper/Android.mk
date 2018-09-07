2b6aee4b04ec9978c12708cabe2852f5bd136f0e
Xianzhong <xianzhong.li@nxp.com>
2018-09-08 05:42:58 +0800

    6.2.4.p3-0050-CL167934-fix-gralloc-helper-build-error-on-old-BSP-M
    CL167934 fix gralloc-helper build error on old BSP Make it
     backward compatible just like IMX-1020
    
    Signed-off-by: Xianzhong <xianzhong.li@nxp.com>

--- driver/driver/android/gralloc_helper/Android.mk
+++ driver/driver/android/gralloc_helper/Android.mk
@@ -12,7 +12,7 @@
 
 
 #
-# libgralloc_helper.so
+# libv_gralloc.so
 #
 LOCAL_PATH := $(call my-dir)
 include $(LOCAL_PATH)/../../../Android.mk.def
@@ -26,14 +26,15 @@ LOCAL_CFLAGS := \
     -DDRM_GRALLOC=1 \
     -DLOG_TAG=\"gralloc-helper\"
 
+ifeq ($(LIBDRM_IMX),1)
 LOCAL_C_INCLUDES := \
-	$(AQROOT)/hal/user \
-	$(AQROOT)/hal/os/linux/user \
-	$(AQROOT)/hal/inc \
-	$(AQROOT)/compiler/libVSC/include \
-	$(AQROOT)/driver/android/gralloc_drm \
-        $(IMX_PATH)/libdrm-imx/vivante \
-        $(IMX_PATH)/libdrm-imx/include/drm
+    $(AQROOT)/hal/user \
+    $(AQROOT)/hal/os/linux/user \
+    $(AQROOT)/hal/inc \
+    $(AQROOT)/compiler/libVSC/include \
+    $(AQROOT)/driver/android/gralloc_drm \
+    $(IMX_PATH)/libdrm-imx/vivante \
+    $(IMX_PATH)/libdrm-imx/include/drm
 
 LOCAL_SHARED_LIBRARIES := \
     libcutils \
@@ -41,6 +42,24 @@ LOCAL_SHARED_LIBRARIES := \
     libdrm_android \
     libdrm_vivante \
     libGAL
+
+else
+LOCAL_C_INCLUDES := \
+    $(AQROOT)/hal/user \
+    $(AQROOT)/hal/os/linux/user \
+    $(AQROOT)/hal/inc \
+    $(AQROOT)/compiler/libVSC/include \
+    $(AQROOT)/driver/android/gralloc_drm \
+    external/libdrm/vivante \
+    external/libdrm/include/drm
+
+LOCAL_SHARED_LIBRARIES := \
+    libcutils \
+    liblog \
+    libdrm \
+    libdrm_vivante \
+    libGAL
+endif
 
 LOCAL_C_INCLUDES += \
     $(IMX_PATH)/imx/include
