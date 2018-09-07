2903aa797a9e24520a7dbbb5d54515784e246a6a
Xianzhong <xianzhong.li@nxp.com>
2018-09-08 05:43:04 +0800

    6.2.4.p3-0084-CL171527-Back-out-revision-5-from-SW-Rel5x-dev6.2.x-
    CL171527 Back out revision 5 from
     //SW/Rel5x/dev6.2.x/projects.dev_134443/hal/kernel/arch/gc_hal_kernel_context.c
    
    Signed-off-by: Xianzhong <xianzhong.li@nxp.com>

--- driver/hal/kernel/arch/gc_hal_kernel_context.c
+++ driver/hal/kernel/arch/gc_hal_kernel_context.c
@@ -2746,11 +2746,6 @@ _DestroyContext(
             Context->buffer = next;
         }
 
-        gcmkVERIFY_OK(gckEVENT_Submit(Context->hardware->kernel->eventObj,
-            gcvTRUE,
-            gcvFALSE
-            ));
-
 #if gcdSECURE_USER
         /* Free the hint array. */
         if (Context->hint != gcvNULL)
