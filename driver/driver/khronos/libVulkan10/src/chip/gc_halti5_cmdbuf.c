f3d2ab51bff9aac903318f717d2063678cb62c38
Xianzhong <xianzhong.li@nxp.com>
2018-11-20 19:12:09 +0800

    MGS-4384-69 [#imx-1215] 0087-IMX-1218-BUG-22071-CL185226-CtsSkQPTestCases-many-ca
    IMX-1218 BUG#22071 CL185226 CtsSkQPTestCases: many case
     failed in subgroup "org.skia.skqp.SkQPRunner#vk_*" on 8qxp
     P9.0/8mscale_P9.0 CL185226 fix part of bug 22071 failed
     cases, buffer memory offset should be added to phyiscal
     address when program commandbuffer.
    
    Signed-off-by: Xianzhong <xianzhong.li@nxp.com>

--- driver/driver/khronos/libVulkan10/src/chip/gc_halti5_cmdbuf.c
+++ driver/driver/khronos/libVulkan10/src/chip/gc_halti5_cmdbuf.c
@@ -5050,7 +5050,7 @@ static VkResult halti5_helper_setDescSetStorage(
                         uint32_t dataCount = 0;
                         __vkBuffer *buf = resInfo->u.bufferInfo.buffer;
                         physical = buf->memory->devAddr;
-                        physical += (uint32_t)resInfo->u.bufferInfo.offset;
+                        physical += (uint32_t)(buf->memOffset + resInfo->u.bufferInfo.offset);
                         data[dataCount++] = physical;
                         if (devCtx->database->ROBUSTNESS)
                         {
@@ -5181,7 +5181,7 @@ static VkResult halti5_helper_setDescSetUniformBuffer(
                         uint32_t dataCount = 0;
                         __vkBuffer *buf = resInfo->u.bufferInfo.buffer;
                         physical = buf->memory->devAddr;
-                        physical += (uint32_t)resInfo->u.bufferInfo.offset;
+                        physical += (uint32_t)(buf->memOffset + resInfo->u.bufferInfo.offset);
                         data[dataCount++] = physical;
                         if (devCtx->database->ROBUSTNESS)
                         {
