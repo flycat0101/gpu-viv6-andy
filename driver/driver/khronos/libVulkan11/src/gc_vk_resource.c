bd9146f33108fd82fcc540172c8d812ce41e755a
Xianzhong <xianzhong.li@nxp.com>
2019-07-27 23:33:16 +0800

    6.4.0.rc2-0074-CL216356-Back-out-changelist-216232-have-regression-
    CL216356 Back out changelist 216232, have regression issue,
     after check will re-check in
    
    Signed-off-by: Xianzhong <xianzhong.li@nxp.com>

--- driver/driver/khronos/libVulkan11/src/gc_vk_resource.c
+++ driver/driver/khronos/libVulkan11/src/gc_vk_resource.c
@@ -2617,8 +2617,16 @@ VKAPI_ATTR VkResult VKAPI_CALL __vk_CreateImage(
         }
         else
         {
-            /* alignment should be 16(pixels) * byte per pixels for tiled surface, and HW require minimum 64 bytes align*/
-            alignment = (img->formatInfo.bitsPerBlock >= 64) ? (4 * 4 * img->formatInfo.bitsPerBlock / 8) : 64;
+            if (img->formatInfo.compressed && img->halTiling == gcvTILED)
+            {
+                /* compressed format is disable supertile */
+                alignment =  img->formatInfo.bitsPerBlock / 8;
+            }
+            else
+            {
+                /* alignment should be 16(pixels) * byte per pixels for tiled surface*/
+                alignment = (img->formatInfo.bitsPerBlock >= 64) ? (4 * 4 * img->formatInfo.bitsPerBlock/8) : 64;
+            }
         }
 
         width  = pCreateInfo->extent.width;
