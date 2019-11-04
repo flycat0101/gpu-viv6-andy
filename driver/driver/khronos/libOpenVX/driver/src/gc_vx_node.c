53379a6b9c7467a88ae8aa8515a62fb40087c1d9
Xianzhong <xianzhong.li@nxp.com>
2019-11-04 19:40:44 +0800

    6.4.0.p2-0318-CL233516-fix-coverity-issue-for-64x
    CL233516 fix coverity issue for 64x
    
    Signed-off-by: Xianzhong <xianzhong.li@nxp.com>

--- driver/driver/khronos/libOpenVX/driver/src/gc_vx_node.c
+++ driver/driver/khronos/libOpenVX/driver/src/gc_vx_node.c
@@ -733,9 +733,9 @@ VX_INTERNAL_API vx_status vxoNode_SetParameter(vx_node node, vx_uint32 index, vx
     if (node->paramTable[index] != VX_NULL)
     {
         char name_tmp[VX_MAX_REFERENCE_NAME];
-        strncpy(name_tmp, ((vx_reference)node->paramTable[index])->name, VX_MAX_REFERENCE_NAME);
+        strncpy(name_tmp, ((vx_reference)node->paramTable[index])->name, VX_MAX_REFERENCE_NAME - 1);
         vxoReference_Release(&node->paramTable[index], node->paramTable[index]->type, VX_REF_INTERNAL);
-        strncpy(value->name, name_tmp, VX_MAX_REFERENCE_NAME);
+        strncpy(value->name, name_tmp, VX_MAX_REFERENCE_NAME - 1);
         vxmASSERT(strlen(name_tmp) < VX_MAX_REFERENCE_NAME);
     }
 
