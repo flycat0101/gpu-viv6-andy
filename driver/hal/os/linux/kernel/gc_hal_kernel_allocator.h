ccc2abd98edbd8e4be08e772a964e91da0cb6578
Xianzhong <xianzhong.li@nxp.com>
2019-11-04 19:28:57 +0800

    6.4.0.p2-0030-CL229827-Merging-CL209976-SW-Rel5x-projects.dev-hal-
    CL229827 Merging CL209976
     //SW/Rel5x/projects.dev/hal/os/linux/kernel/... to
     //SW/Rel5x/dev6.4.x/projects.dev/hal/os/linux/kernel/...
    
    Signed-off-by: Xianzhong <xianzhong.li@nxp.com>

--- driver/hal/os/linux/kernel/gc_hal_kernel_allocator.h
+++ driver/hal/os/linux/kernel/gc_hal_kernel_allocator.h
@@ -399,35 +399,38 @@ typedef struct _gcsALLOCATOR_OPERATIONS
 gcsALLOCATOR_OPERATIONS;
 
 /* defination of allocator operations wrapper*/
-#define gcmALLOCTATOR_Alloc(Allocator, Mdl, NumPages, Flag)     \
-            (Allocator)->ops->Alloc(Allocator, Mdl, NumPages, Flag)
+#define gcmALLOCATOR_Alloc(Allocator, Mdl, NumPages, Flag)     \
+            (Allocator)->ops->Alloc((Allocator), (Mdl), (NumPages), (Flag))
 
-#define gcmALLOCTATOR_Free(Allocator, Mdl)      \
-            (Allocator)->ops->Free(Allocator, Mdl)
+#define gcmALLOCATOR_Free(Allocator, Mdl)      \
+            (Allocator)->ops->Free((Allocator), (Mdl))
 
-#define gcmALLOCTATOR_Mmap(Allocator, Mdl, Cacheable, skipPages, numPages, vma) \
-            (Allocator)->ops->Mmap(Allocator, Mdl, Cacheable, skipPages, numPages, vma)
+#define gcmALLOCATOR_Mmap(Allocator, Mdl, Cacheable, skipPages, numPages, vma) \
+            (Allocator)->ops->Mmap((Allocator), (Mdl), (Cacheable), (skipPages), (numPages), (vma))
 
-#define gcmALLOCTATOR_MapUser(Allocator, Mdl, MdlMap, Cacheable)    \
-            (Allocator)->ops->MapUser(Allocator, Mdl, MdlMap, Cacheable)
+#define gcmALLOCATOR_MapUser(Allocator, Mdl, MdlMap, Cacheable)    \
+            (Allocator)->ops->MapUser((Allocator), (Mdl), (MdlMap), (Cacheable))
 
-#define gcmALLOCTATOR_UnmapUser(Allocator, Mdl, MdlMap, Size)   \
-            (Allocator)->ops->UnmapUser(Allocator, Mdl, MdlMap, Size)
+#define gcmALLOCATOR_UnmapUser(Allocator, Mdl, MdlMap, Size)   \
+            (Allocator)->ops->UnmapUser((Allocator), (Mdl), (MdlMap), (Size))
 
-#define gcmALLOCTATOR_MapKernel(Allocator, Mdl, Offset, Bytes, Logical) \
-            (Allocator)->ops->MapKernel(Allocator, Mdl, Offset, Bytes, Logical)
+#define gcmALLOCATOR_MapKernel(Allocator, Mdl, Offset, Bytes, Logical) \
+            (Allocator)->ops->MapKernel((Allocator), (Mdl), (Offset), (Bytes), (Logical))
 
-#define gcmALLOCTATOR_Cache(Allocator, Mdl, Offset, Logical, Bytes, Operation)  \
-            (Allocator)->ops->Cache(Allocator, Mdl, Offset, Logical, Bytes, Operation)
+#define gcmALLOCATOR_UnmapKernel(Allocator, Mdl, Logical) \
+            (Allocator)->ops->UnmapKernel((Allocator), (Mdl), (Logical))
 
-#define gcmALLOCTATOR_Physical(Allocator, Mdl, Offset, Physical)    \
-            (Allocator)->ops->Physical(Allocator, Mdl, Offset, Physical)
+#define gcmALLOCATOR_Cache(Allocator, Mdl, Offset, Logical, Bytes, Operation)  \
+            (Allocator)->ops->Cache((Allocator), (Mdl), (Offset), (Logical), (Bytes), (Operation))
 
-#define gcmALLOCTATOR_Attach(Allocator, Desc, Mdl)  \
-            (Allocator)->ops->Attach(Allocator, Desc, Mdl)
+#define gcmALLOCATOR_Physical(Allocator, Mdl, Offset, Phys)    \
+            (Allocator)->ops->Physical((Allocator), (Mdl), (Offset), (Phys))
 
-#define gcmALLOCTATOR_GetSGT(Allocator, Mdl, Offset, Bytes, SGT) \
-            (Allocator)->ops->GetSGT(Allocator, Mdl, Offset, Bytes, SGT)
+#define gcmALLOCATOR_Attach(Allocator, Desc, Mdl)  \
+            (Allocator)->ops->Attach((Allocator), (Desc), (Mdl))
+
+#define gcmALLOCATOR_GetSGT(Allocator, Mdl, Offset, Bytes, SGT) \
+            (Allocator)->ops->GetSGT((Allocator), (Mdl), (Offset), (Bytes), (SGT))
 
 typedef struct _gcsALLOCATOR
 {
--- driver/hal/os/linux/kernel/gc_hal_kernel_os.c
+++ driver/hal/os/linux/kernel/gc_hal_kernel_os.c
@@ -236,9 +236,9 @@ _DestroyMdl(
         {
             if (Mdl->addr)
             {
-                allocator->ops->UnmapKernel(allocator, Mdl, Mdl->addr);
+                gcmALLOCATOR_UnmapKernel(allocator, Mdl, Mdl->addr);
             }
-            allocator->ops->Free(allocator, Mdl);
+            gcmALLOCATOR_Free(allocator, Mdl);
         }
 
         mutex_lock(&Mdl->mapsMutex);
@@ -875,7 +875,7 @@ gckOS_CreateKernelMapping(
     }
     else
     {
-        gcmkONERROR(allocator->ops->MapKernel(allocator, mdl, Offset, Bytes, Logical));
+        gcmkONERROR(gcmALLOCATOR_MapKernel(allocator, mdl, Offset, Bytes, Logical));
     }
 
 OnError:
@@ -901,7 +901,7 @@ gckOS_DestroyKernelMapping(
     }
     else
     {
-        allocator->ops->UnmapKernel(allocator, mdl, Logical);
+        gcmALLOCATOR_UnmapKernel(allocator, mdl, Logical);
     }
 
     gcmkFOOTER_NO();
@@ -1156,10 +1156,7 @@ gckOS_MapMemory(
     {
         allocator = mdl->allocator;
 
-        gcmkONERROR(
-            allocator->ops->MapUser(allocator,
-                                    mdl, mdlMap,
-                                    gcvFALSE));
+        gcmkONERROR(gcmALLOCATOR_MapUser(allocator, mdl, mdlMap, gcvFALSE));
     }
 
     mutex_unlock(&mdl->mapsMutex);
@@ -1290,7 +1287,7 @@ gckOS_UnmapMemoryEx(
 
         BUG_ON(!allocator || !allocator->ops->UnmapUser);
 
-        allocator->ops->UnmapUser(allocator, mdl, mdlMap, mdl->bytes);
+        gcmALLOCATOR_UnmapUser(allocator, mdl, mdlMap, mdl->bytes);
 
         gcmkVERIFY_OK(_DestroyMdlMap(mdl, mdlMap));
 
@@ -1398,7 +1395,7 @@ gckOS_AllocateNonPagedMemory(
              * allocators when page count is 1. This is to save memory usage of
              * dma coherent pool.
              */
-            status = allocator->ops->Alloc(allocator, mdl, numPages, Flag);
+            status = gcmALLOCATOR_Alloc(allocator, mdl, numPages, Flag);
 
             if (gcmIS_SUCCESS(status))
             {
@@ -1409,7 +1406,7 @@ gckOS_AllocateNonPagedMemory(
 #else
         if ((Flag & allocator->capability) == Flag)
         {
-            status = allocator->ops->Alloc(allocator, mdl, numPages, Flag);
+            status = gcmALLOCATOR_Alloc(allocator, mdl, numPages, Flag);
 
             if (gcmIS_SUCCESS(status))
             {
@@ -1430,7 +1427,7 @@ gckOS_AllocateNonPagedMemory(
 
     mdl->contiguous = gcvTRUE;
 
-    gcmkONERROR(allocator->ops->MapKernel(allocator, mdl, 0, bytes, &addr));
+    gcmkONERROR(gcmALLOCATOR_MapKernel(allocator, mdl, 0, bytes, &addr));
 
     if (!strcmp(allocator->name, "gfp"))
     {
@@ -1449,7 +1446,7 @@ gckOS_AllocateNonPagedMemory(
             gcmkONERROR(gcvSTATUS_OUT_OF_MEMORY);
         }
 
-        gcmkONERROR(allocator->ops->MapUser(allocator, mdl, mdlMap, gcvFALSE));
+        gcmkONERROR(gcmALLOCATOR_MapUser(allocator, mdl, mdlMap, gcvFALSE));
 
         *Logical = mdlMap->vmaAddr;
     }
@@ -1603,7 +1600,7 @@ gckOS_RequestReservedMemory(
     }
 
     /* Call attach. */
-    gcmkONERROR(allocator->ops->Attach(allocator, &desc, mdl));
+    gcmkONERROR(gcmALLOCATOR_Attach(allocator, &desc, mdl));
 
     /* Assign alloator. */
     mdl->allocator  = allocator;
@@ -1652,7 +1649,7 @@ gckOS_ReleaseReservedMemory(
     /* If no allocator, how comes the memory? */
     BUG_ON(!allocator);
 
-    allocator->ops->Free(allocator, mdl);
+    gcmALLOCATOR_Free(allocator, mdl);
 }
 
 /*******************************************************************************
@@ -2079,7 +2076,7 @@ gckOS_GetPhysicalFromHandle(
     PLINUX_MDL mdl = (PLINUX_MDL)Physical;
     gckALLOCATOR allocator = mdl->allocator;
 
-    return allocator->ops->Physical(allocator, mdl, Offset, PhysicalAddress);
+    return gcmALLOCATOR_Physical(allocator, mdl, Offset, PhysicalAddress);
 }
 
 
@@ -2148,7 +2145,7 @@ _ConvertLogical2Physical(
     {
         offset = (gctINT8_PTR) Logical - vBase;
 
-        allocator->ops->Physical(allocator, Mdl, offset, Physical);
+        gcmALLOCATOR_Physical(allocator, Mdl, offset, Physical);
 
         status = gcvSTATUS_OK;
     }
@@ -3131,7 +3128,7 @@ gckOS_AllocatePagedMemory(
             continue;
         }
 
-        status = allocator->ops->Alloc(allocator, mdl, numPages, Flag);
+        status = gcmALLOCATOR_Alloc(allocator, mdl, numPages, Flag);
 
         if (gcmIS_SUCCESS(status))
         {
@@ -3296,7 +3293,7 @@ gckOS_LockPages(
 
     if (mdlMap->vmaAddr == gcvNULL)
     {
-        gcmkONERROR(allocator->ops->MapUser(allocator, mdl, mdlMap, Cacheable));
+        gcmkONERROR(gcmALLOCATOR_MapUser(allocator, mdl, mdlMap, Cacheable));
     }
 
     mdlMap->count++;
@@ -3383,7 +3380,7 @@ gckOS_MapPagesEx(
         gctUINT i;
         gctPHYS_ADDR_T phys = ~0U;
 
-        allocator->ops->Physical(allocator, mdl, offset, &phys);
+        gcmALLOCATOR_Physical(allocator, mdl, offset, &phys);
 
         gcmkVERIFY_OK(gckOS_CPUPhysicalToGPUPhysical(Os, phys, &phys));
 
@@ -3588,7 +3585,7 @@ gckOS_Map1MPages(
     {
         gctPHYS_ADDR_T phys = ~0U;
 
-        allocator->ops->Physical(allocator, mdl, offset, &phys);
+        gcmALLOCATOR_Physical(allocator, mdl, offset, &phys);
 
         gcmkVERIFY_OK(gckOS_CPUPhysicalToGPUPhysical(Os, phys, &phys));
 
@@ -3704,7 +3701,7 @@ gckOS_UnlockPages(
         {
             if (--mdlMap->count == 0)
             {
-                allocator->ops->UnmapUser(
+                gcmALLOCATOR_UnmapUser(
                     allocator,
                     mdl,
                     mdlMap,
@@ -4225,7 +4222,7 @@ _CacheOperation(
         if ((!ProcessID && mdl->cacheable) ||
             (mdlMap && mdlMap->cacheable))
         {
-            allocator->ops->Cache(allocator,
+            gcmALLOCATOR_Cache(allocator,
                 mdl, Offset, Logical, Bytes, Operation);
 
             return gcvSTATUS_OK;
@@ -7006,7 +7003,7 @@ gckOS_AllocatePageArray(
 
         gctPHYS_ADDR_T phys_addr;
 
-        allocator->ops->Physical(allocator, mdl, offset * PAGE_SIZE, &phys_addr);
+        gcmALLOCATOR_Physical(allocator, mdl, offset * PAGE_SIZE, &phys_addr);
 
         phys = (unsigned long)phys_addr;
 
@@ -7244,7 +7241,7 @@ gckOS_MemoryGetSGT(
 
     if (Bytes > 0)
     {
-        gcmkONERROR(allocator->ops->GetSGT(allocator, mdl, Offset, Bytes, SGT));
+        gcmkONERROR(gcmALLOCATOR_GetSGT(allocator, mdl, Offset, Bytes, SGT));
     }
 
 OnError:
@@ -7289,7 +7286,7 @@ gckOS_MemoryMmap(
 
     mutex_unlock(&mdl->mapsMutex);
 
-    gcmkONERROR(allocator->ops->Mmap(allocator, mdl, cacheable, skipPages, numPages, Vma));
+    gcmkONERROR(gcmALLOCATOR_Mmap(allocator, mdl, cacheable, skipPages, numPages, Vma));
 
 OnError:
     return status;
@@ -7398,7 +7395,7 @@ gckOS_WrapMemory(
             }
         }
 
-        status = allocator->ops->Attach(allocator, &desc, mdl);
+        status = gcmALLOCATOR_Attach(allocator, &desc, mdl);
 
         if (gcmIS_SUCCESS(status))
         {
