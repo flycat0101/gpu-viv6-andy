b067f37a278c6c83125d75ae7c281db99539d7af
Xiaojun.Chen <xiaojun.chen@verisilicon.com>
2019-11-21 14:44:03 +0800

    IMX-1769 BUG25182 CL235875 [NXP]:Unit test of Arm-NN failed at SoftRelu node

--- driver/driver/khronos/libOpenVX/driver/include/gc_vx_layer.h
+++ driver/driver/khronos/libOpenVX/driver/include/gc_vx_layer.h
@@ -4086,6 +4086,17 @@ vxnne_shader_executable vxnneGPUTensorCopyROIShaderExecutable(
     vx_scalar               outputYOffset,
     vx_tensor               input,
     vx_tensor               output);
+
+vxnne_shader_executable vxnneGetGPUTensorTRShaderExecutable(
+    vx_context              context,
+    vx_enum                 kernelEnum,
+    vx_border_mode_t        *borderMode,
+    vx_tensor               inputs,
+    vx_float32              a_val,
+    vx_float32              b_val,
+    vx_enum                 funcType,
+    vx_tensor               outputs);
+
 #ifdef __cplusplus
 }
 #endif
