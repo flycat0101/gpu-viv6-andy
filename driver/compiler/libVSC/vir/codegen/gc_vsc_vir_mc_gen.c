aee3696bbf92a151bf988b8d7c3d11215d34c3b8
Xiaojun.Chen <xiaojun.chen@verisilicon.com>
2019-12-12 11:23:40 +0800

    IMX-1827 BUG25537 CL242055 [NXP]:6.4.0.p2: OpenCL CTS math failed on 6QP. CL242055 It looks like there are some timing issues for the ATOM instruction in 6QP, now compiler inserts NOPs after a ATOM instruction to avoid this issue, need HW team to find the root cause.

--- driver/compiler/libVSC/vir/codegen/gc_vsc_vir_mc_gen.c
+++ driver/compiler/libVSC/vir/codegen/gc_vsc_vir_mc_gen.c
@@ -2944,6 +2944,7 @@ _VSC_MC_GEN_GenInst(
     gctUINT                 srcNum       = 0;
     gctBOOL                 bDstWrite    = gcvFALSE;
     gctBOOL                 bNeedGen     = gcvTRUE;
+    gctBOOL                 bForceGen    = (VIR_Inst_GetFlags(Inst) & VIR_INSTFLAG_FORCE_GEN) ? gcvTRUE : gcvFALSE;
 
     VSC_MC_CODEC_INST_CTRL  mcInstCtrl;
     VSC_MC_CODEC_DST        mcDest;
@@ -2973,7 +2974,10 @@ _VSC_MC_GEN_GenInst(
         *GenCount = 0;
         break;
     case VIR_OP_NOP:
-        *GenCount = 0;
+        if (!bForceGen)
+        {
+            *GenCount = 0;
+        }
         break;
     case VIR_OP_JMP:
     case VIR_OP_JMPC:
