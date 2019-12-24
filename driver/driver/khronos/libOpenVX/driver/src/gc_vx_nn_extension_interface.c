4eab218ab27ec259dbb7d931441c7875811959a8
Xiaojun.Chen <xiaojun.chen@verisilicon.com>
2019-12-24 19:25:10 +0800

    CL244725 [openvx]:Reset VX node's layer to be null if the layer failed to be created

--- driver/driver/khronos/libOpenVX/driver/src/gc_vx_nn_extension_interface.c
+++ driver/driver/khronos/libOpenVX/driver/src/gc_vx_nn_extension_interface.c
@@ -3407,8 +3407,10 @@ vx_status vxnneLayer_Ops_Initialize(
     return status;
 
 OnError:
-    if (node->layer)
+    if (node->layer) {
         gcoOS_Free(gcvNULL, node->layer);
+        node->layer = VX_NULL;
+    }
 
     return status;
 }
