a92e213d572b54f15f7f22e7607647208177d2bf
Xiaojun.Chen <xiaojun.chen@verisilicon.com>
2020-01-15 13:44:31 +0800

    IMX-1142 CL248016 A quick solution for issue "[imx8qxp] Can not pass ES31 CTS test". Will improve the code later.

--- driver/compiler/libVSC/lib/gc_vsc_lib_common.c
+++ driver/compiler/libVSC/lib/gc_vsc_lib_common.c
@@ -3190,8 +3190,9 @@ static gceSTATUS gcSHADER_GetTempFileName(IN gctBOOL     isPatch,
 {
     gceSTATUS status = gcvSTATUS_OK;
     gctCHAR gcTmpFileName[_cldFILENAME_MAX+1];
-
+    gcePATCH_ID patchId = gcPatchId;
     gctSTRING env = gcvNULL;
+
     gcoOS_GetEnv(gcvNULL, "VIV_LIB_SHADER_DIR", &env);
     if (env)
     {
@@ -3210,6 +3211,10 @@ static gceSTATUS gcSHADER_GetTempFileName(IN gctBOOL     isPatch,
         _cldFILENAME_MAX,
         "/"));
 #endif
+    if (isAppConformance(patchId))
+    {
+        gcmONERROR(gcoOS_StrCatSafe(gcTmpFileName, _cldFILENAME_MAX, "cts_"));
+    }
 
     if (isPatch)
     {
