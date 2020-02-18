461187bf188827aec32768f6fbb650c9607d280d
Xianzhong <xianzhong.li@nxp.com>
2020-02-19 00:32:57 +0800

    6.4.3.p0 restore VXC kernel from 6.4.0.p2
    move VXC integration to viv_6.4.4.p0 branch
    
    Signed-off-by: Xianzhong <xianzhong.li@nxp.com>

--- driver/driver/khronos/libOpenVX/libkernel/libnngpu/nngpu_binary_interface.h
+++ driver/driver/khronos/libOpenVX/libkernel/libnngpu/nngpu_binary_interface.h
@@ -1,45 +1,21 @@
+/****************************************************************************
+*
+*    Copyright (c) 2005 - 2019 by Vivante Corp.  All rights reserved.
+*
+*    The material in this file is confidential and contains trade secrets
+*    of Vivante Corporation. This is proprietary information owned by
+*    Vivante Corporation. No part of this work may be disclosed,
+*    reproduced, copied, transmitted, or used in any way for any purpose,
+*    without the express written permission of Vivante Corporation.
+*
+*****************************************************************************/
+
+
 #ifndef __NN_GPU_BINARY_INTERFACE_H__
 #define __NN_GPU_BINARY_INTERFACE_H__
 
 typedef enum _nngpu_kernel_enum
 {
-    TensorPad,
-    TensorStridedSlice,
-    EmbeddingLUT,
-    Tensor2Row,
-    MaxPooling,
-    TensorTranspose,
-    Space2Batch,
-    Space2Depth,
-    DepthwiseConv,
-    L2NormSumSqrt,
-    Depth2Space,
-    TensorScale,
-    Activation,
-    TensorElewise,
-    Reverse,
-    Conv2D_1x1,
-    TensorReduceDiv,
-    TensorMaxValue,
-    L2NormSumScale,
-    LSTMUnit,
-    Svdf,
-    TensorMeanAxis0,
-    TensorCrop,
-    Gemm_noBias,
-    Rnn,
-    AvgPooling,
-    SoftMax,
-    LSTMUnitProjection,
-    TensorCopyROI,
-    TensorCopy,
-    L2Pooling,
-    FullyConnected,
-    HashLUT,
-    Normalization,
-    Gemm,
-    Batch2Space,
-    Floor,
     NNGPU_KERNEL_NUM /*NNVXC_KERNEL_NUM should be the last item in the enum*/
 }
 nngpu_kernel_enum;
