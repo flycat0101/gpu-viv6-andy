622f96f29d448b8c46fe260ebadb6adb4654401a
Xianzhong <xianzhong.li@nxp.com>
2020-02-19 00:32:57 +0800

    6.4.3.p0 restore VXC kernel from 6.4.0.p2
    move VXC integration to viv_6.4.4.p0 branch
    
    Signed-off-by: Xianzhong <xianzhong.li@nxp.com>

--- driver/driver/khronos/libOpenVX/libkernel/libovx12/ovx12_vxc_binary_interface.h
+++ driver/driver/khronos/libOpenVX/libkernel/libovx12/ovx12_vxc_binary_interface.h
@@ -1,34 +1,21 @@
+/****************************************************************************
+*
+*    Copyright (c) 2005 - 2019 by Vivante Corp.  All rights reserved.
+*
+*    The material in this file is confidential and contains trade secrets
+*    of Vivante Corporation. This is proprietary information owned by
+*    Vivante Corporation. No part of this work may be disclosed,
+*    reproduced, copied, transmitted, or used in any way for any purpose,
+*    without the express written permission of Vivante Corporation.
+*
+*****************************************************************************/
+
+
 #ifndef __OVX12_VXC_BINARY_INTERFACE_H__
 #define __OVX12_VXC_BINARY_INTERFACE_H__
 
 typedef enum _ovx12_vxc_kernel_enum
 {
-    match_template,
-    tensor_convert_depth,
-    copy,
-    non_max_suppression,
-    transpose_2d_tensor,
-    tensorlut,
-    hog_features,
-    bilateral_filter,
-    scalarCopy,
-    lutCopy,
-    fillaccum,
-    getlines,
-    pyramid_copy_image,
-    max,
-    upsample_convert,
-    min,
-    lbp,
-    arrayCopy,
-    scalar_operation,
-    imageCopy,
-    hog_cells,
-    multiply_2d_matrixes,
-    threshold,
-    upsample_padding,
-    makepoints,
-    remapCopy,
     OVX12_VXC_KERNEL_NUM /*OVX12_VXC_KERNEL_NUM should be the last item in the enum*/
 }
 ovx12_vxc_kernel_enum;
