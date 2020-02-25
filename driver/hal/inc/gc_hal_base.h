0330aeebc5e2a9e26a7df9938e26b74f26d96eef
Xianzhong <xianzhong.li@nxp.com>
2020-02-26 00:54:28 +0800

    6.4.1-0636-CL244484-dev6.4.x-dev6.4.x_234062-fixed-25711-25710-
    CL244484 dev6.4.x/ dev6.4.x_234062 fixed
     #25711/25710/25646, For the failed cases, if the attribufer buffer size is
     less 4 page, the program link will fail due to insufficient attribute buffer
     resources. So decrease the L1 cache size.
    
    Signed-off-by: Xianzhong <xianzhong.li@nxp.com>

--- driver/hal/inc/gc_hal_base.h
+++ driver/hal/inc/gc_hal_base.h
@@ -5959,8 +5959,8 @@ gcoHAL_GetUserDebugOption(
                 } \
                 else \
                 { \
-                    attribBufSizeInKB -= 4; \
-                    L1cacheSize = 4; \
+                    attribBufSizeInKB -= 2; \
+                    L1cacheSize = 2; \
                 } \
             } \
             prefix##ASSERT(L1cacheSize); \
