d144410c666203cb300f0e65ed48cb7068648615
Xianzhong <xianzhong.li@nxp.com>
2020-02-26 00:51:00 +0800

    6.4.1-0003-CL234143-Backout-the-change
    CL234143 Backout the change.
    
    Signed-off-by: Xianzhong <xianzhong.li@nxp.com>

--- driver/hal/user/gc_hal_user_raster.c
+++ driver/hal/user/gc_hal_user_raster.c
@@ -234,6 +234,10 @@ static gceSTATUS _CheckSurface(
     case gcvSUPERTILED:
     case gcvMULTI_TILED:
     case gcvMULTI_SUPERTILED:
+        if (!Engine->tiling)
+        {
+            return gcvSTATUS_NOT_SUPPORTED;
+        }
         break;
 
     case gcvMINORTILED:
