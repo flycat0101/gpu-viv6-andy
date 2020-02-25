3a728100312325b5d9e6312b2d5a5354406d242f
Xianzhong <xianzhong.li@nxp.com>
2020-02-26 00:52:07 +0800

    6.4.1-0214-CL237049-6.4.x-Fix-QNX-build-error-caused-by-CL23674
    CL237049 6.4.x: Fix QNX build error caused by
     CL236742.
    
    Signed-off-by: Xianzhong <xianzhong.li@nxp.com>

--- driver/build_qnx/vx/common.mk
+++ driver/build_qnx/vx/common.mk
@@ -131,6 +131,7 @@ SOURCE_OBJECTS += $(driver_root)/driver/khronos/libOpenVX/driver/src/gc_vx_layer
 SOURCE_OBJECTS += $(driver_root)/driver/khronos/libOpenVX/driver/src/gc_vx_gpu_layer.o
 SOURCE_OBJECTS += $(driver_root)/driver/khronos/libOpenVX/driver/src/gc_vx_nn_util.o
 SOURCE_OBJECTS += $(driver_root)/driver/khronos/libOpenVX/driver/src/gc_vx_nn_encoder.o
+SOURCE_OBJECTS += $(driver_root)/driver/khronos/libOpenVX/driver/src/gc_vx_nn_wb.o
 SOURCE_OBJECTS += $(driver_root)/driver/khronos/libOpenVX/driver/src/gc_vx_nn_command.o
 SOURCE_OBJECTS += $(driver_root)/driver/khronos/libOpenVX/driver/src/gc_vx_binary.o
 
