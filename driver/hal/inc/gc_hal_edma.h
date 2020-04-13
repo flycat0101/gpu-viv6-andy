f2062f75c2925e7943632b8a01293247b44ef619
Xianzhong <xianzhong.li@nxp.com>
2020-04-13 18:25:00 +0800

    6.4.2-0399-CL252246-Merging-CL221494-SW-Rel5x-projects.dev-hal-
    CL252246 Merging CL221494
     //SW/Rel5x/projects.dev/hal/... to //SW/Rel5x/projects.dev/hal/...
    
    Signed-off-by: Xianzhong <xianzhong.li@nxp.com>

---
+++ driver/hal/inc/gc_hal_edma.h
@@ -0,0 +1,55 @@
+/****************************************************************************
+*
+*    Copyright (c) 2005 - 2020 by Vivante Corp.  All rights reserved.
+*
+*    The material in this file is confidential and contains trade secrets
+*    of Vivante Corporation. This is proprietary information owned by
+*    Vivante Corporation. No part of this work may be disclosed,
+*    reproduced, copied, transmitted, or used in any way for any purpose,
+*    without the express written permission of Vivante Corporation.
+*
+*****************************************************************************/
+
+
+#include "gc_hal_types.h"
+
+
+typedef unsigned int handle_t;
+
+typedef enum
+{
+    gcvEDMA_IOCTL_CMD_GET_CHAN = 0,
+    gcvEDMA_IOCTL_CMD_CFG_AND_SUBMIT,
+    gcvEDMA_IOCTL_CMD_START,
+    gcvEDMA_IOCTL_CMD_GET_STATUS,
+    gcvEDMA_IOCTL_CMD_TERMINATE_ALL,
+    gcvEDMA_IOCTL_CMD_RELEASE,
+}gcvEDMA_IOCTL_CMD;
+
+typedef enum
+{
+    gcvDMA_UNUSE = 0,
+    gcvDMA_USED,
+}gcvEDMA_STATE;
+
+typedef enum
+{
+    gcvEDMA_MEM_TO_DEV = 1,
+    gcvEDMA_DEV_TO_MEM,
+}gcvEDMA_DIRECTION;
+
+typedef struct
+{
+    IN gcvEDMA_IOCTL_CMD cmd;
+
+    INOUT handle_t edma_chan;
+    IN gctUINT64 src;
+    IN gctUINT64 dst;
+    INOUT size_t len;
+    IN gcvEDMA_DIRECTION dir;
+    IN bool sync_wait;
+    OUT gctUINT32 edma_status;
+}gcsEDMA_TRANSFACTION;
+
+
+
