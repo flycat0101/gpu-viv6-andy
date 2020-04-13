f9bbe3d6f7b5bf0dcd4779a01af11fd7cc886c0d
Xianzhong <xianzhong.li@nxp.com>
2020-04-13 18:25:41 +0800

    6.4.2-0538-CL254046-Fixed-issues-running-GL4-application-on-EGL
    CL254046 Fixed issues running GL4 application on
     EGL_API_WL and GL_API_DRI build configuration.
    
    Signed-off-by: Xianzhong <xianzhong.li@nxp.com>

--- driver/driver/khronos/libEGL/source/gc_egl_context.c
+++ driver/driver/khronos/libEGL/source/gc_egl_context.c
@@ -49,11 +49,15 @@ static const char * _driverDlls[] =
     "libOpenGL.dylib",                  /* OpenGL */
     "libOpenVG.dylib",                  /* OpenVG 1.0 */
 #else
-    "libEGL",                           /* EGL */
-    "libGLESv1_CM",                     /* OpenGL ES 1.1 Common */
-    "libGLESv2",                        /* OpenGL ES 2.0/3.x */
-    "libGL",                            /* OpenGL */
-    "libOpenVG",                        /* OpenVG 1.0 */
+    "libEGL.so",                        /* EGL */
+    "libGLESv1_CM.so",                  /* OpenGL ES 1.1 Common */
+    "libGLESv2.so",                     /* OpenGL ES 2.0/3.x */
+  #if defined(EGL_API_DRI) || defined(X11_DRI3)
+    "/usr/lib/dri/vivante_dri.so",      /* OpenGL */
+  #else
+    "libGL.so",                         /* OpenGL */
+  #endif
+    "libOpenVG.so",                     /* OpenVG 1.0 */
 #endif
 };
 
