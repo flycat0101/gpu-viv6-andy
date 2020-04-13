07b0766a570c1195895f20e0f2e8b9894d0d3371
Xianzhong <xianzhong.li@nxp.com>
2020-04-13 18:25:33 +0800

    6.4.2-0511-CL253781-Merge-NXP-IMX-1967-patch-to-fix-a-problem-w
    CL253781 Merge NXP IMX-1967 patch to fix a problem
     with Vertex Buffer Object.
    
    Signed-off-by: Xianzhong <xianzhong.li@nxp.com>

--- driver/driver/khronos/libGL4/src/glcore/gc_es_varray.c
+++ driver/driver/khronos/libGL4/src/glcore/gc_es_varray.c
@@ -2555,7 +2555,7 @@ OnError:
 GLvoid GL_APIENTRY __glim_BindVertexBuffer(__GLcontext *gc, GLuint bindingindex, GLuint buffer,
                                            GLintptr offset, GLsizei stride)
 {
-    __GLbufferObject *bufObj;
+    __GLbufferObject *bufObj, *oldBufObj;
     __GLvertexAttribBinding *pAttribBinding;
 
     __GL_HEADER();
@@ -2614,11 +2614,10 @@ GLvoid GL_APIENTRY __glim_BindVertexBuffer(__GLcontext *gc, GLuint bindingindex,
     }
 
     pAttribBinding = &gc->vertexArray.boundVAO->vertexArray.attributeBinding[bindingindex];
+    oldBufObj = pAttribBinding->boundArrayObj;
 
-    if (pAttribBinding->boundArrayName != buffer)
+    if (pAttribBinding->boundArrayName != buffer || (oldBufObj && oldBufObj != bufObj))
     {
-        __GLbufferObject *oldBufObj = pAttribBinding->boundArrayObj;
-
         /* Remove current VAO from old buffer object's vaoList */
         if (oldBufObj)
         {
