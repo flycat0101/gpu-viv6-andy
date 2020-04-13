fb26426658a737e2cc2c7da89125a055910f67d3
Xianzhong <xianzhong.li@nxp.com>
2020-04-13 18:22:59 +0800

    6.4.2-0082-CL246128-dev64x-dev500-fix-the-cl246105-build-error
    CL246128 dev64x/dev500: fix the cl246105 build error
    
    Signed-off-by: Xianzhong <xianzhong.li@nxp.com>

--- driver/hal/kernel/arch/gc_hal_kernel_hardware_func.c
+++ driver/hal/kernel/arch/gc_hal_kernel_hardware_func.c
@@ -10006,12 +10006,12 @@ _FuncInit_MMU(IN gcsFUNCTION_EXECUTION_PTR Execution)
                   "please rebuild kernel with CONFIG_ZONE_DMA32=y or CONFIG_ZONE_DMA=y or both.",
                   __FUNCTION__, __LINE__, physical);
 
-        gcmkFATAL("Some Archs, for ARM64, the setting is special:\n
-                    kernel version   ZONE_DMA   ZONE_DMA32\n
-                    3.7  - 3.14        no          yes\n
-                    3.15 - 4.15        yes         no\n
-                    4.16 - 5.4.5       no          yes\n
-                    5.5.rc1 -          yes         yes\n");
+        gcmkFATAL("Some Archs, for ARM64, the setting is special:\n"
+                    "kernel version   ZONE_DMA   ZONE_DMA32\n"
+                    "3.7  - 3.14        no          yes\n"
+                    "3.15 - 4.15        yes         no\n"
+                    "4.16 - 5.4.5       no          yes\n"
+                    "5.5.rc1 -          yes         yes\n");
 
         gcmkONERROR(gcvSTATUS_OUT_OF_MEMORY);
     }
