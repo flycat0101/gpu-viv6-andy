9bf3f8ad2f7ad0dbf3ab6fbc5429fcb504e0ea45
Xiaojun.Chen <xiaojun.chen@verisilicon.com>
2020-07-01 14:28:38 +0800

    CL276059 Fix bug in _InsertLDARR(), the newVirReg's count is aways "1". According to "long" variable, the count should be "2". It will cause wrong MC code about "long" To verify endian type in gcSHADER_LoadHeader and _gcLoadProgramHeader. for ocl_debug, the linetable number is wrong. The LineNo was set to "0" when "long4" had been initialized with constant. Now set it to right number.

--- driver/compiler/libVSC/old_impl/gc_vsc_old_compiler.c
+++ driver/compiler/libVSC/old_impl/gc_vsc_old_compiler.c
@@ -3907,7 +3907,13 @@ gcSHADER_LoadHeader(
 
     /* Verify the signature. */
     signature = (gceOBJECT_TYPE *) Buffer;
-    if (*signature != gcvOBJ_SHADER) {
+    if (*signature == gcmCC('R','D','H','S')) {
+        /* Endian mismatch. */
+        gcoOS_Print("gcSHADER_LoadHeader: Memory endian type does not match!");
+        gcmFOOTER_ARG("status=%d", gcvSTATUS_INVALID_DATA);
+        return gcvSTATUS_INVALID_DATA;
+    }
+    else if (*signature != gcvOBJ_SHADER) {
         /* Signature mismatch. */
         gcoOS_Print("gcSHADER_LoadHeader: Signature does not match with 'SHDR'");
         gcmFOOTER_ARG("status=%d", gcvSTATUS_INVALID_DATA);
@@ -39128,7 +39134,13 @@ _gcLoadProgramHeader(
 
     /* Verify the signature. */
     signature = (gctUINT32 *) Buffer;
-    if (*signature != gcmCC('P', 'R', 'G', 'M')) {
+    if (*signature == gcmCC('M', 'G', 'R', 'P')) {
+        /* Endian mismatch. */
+        gcoOS_Print("_gcLoadProgramHeader: Memory endian type does not match!");
+        gcmFOOTER_ARG("status=%d", gcvSTATUS_INVALID_DATA);
+        return gcvSTATUS_INVALID_DATA;
+    }
+    else if (*signature != gcmCC('P', 'R', 'G', 'M')) {
         /* Signature mismatch. */
         gcoOS_Print("_gcLoadProgramHeader: Signature does not match with 'PRGM'");
         gcmFOOTER_ARG("status=%d", gcvSTATUS_INVALID_DATA);
