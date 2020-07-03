086e5eee6fc4046be46749790ec318c350d6cfd2
Xiaojun.Chen <xiaojun.chen@verisilicon.com>
2020-07-03 19:36:34 +0800

    CL277275 Update build_sample* scripts to be compatible with the new makefile system. Remove some useless build scripts.

--- driver/driver_build_sample.sh
+++ driver/driver_build_sample.sh
@@ -12,7 +12,6 @@ fi
 
 export AQROOT=`pwd`
 export AQARCH=$AQROOT/arch/XAQ2
-export AQVGARCH=$AQROOT/arch/GC350
 
 export SDK_DIR=$AQROOT/build/sdk
 
@@ -22,26 +21,10 @@ arm)
     export ARCH_TYPE=$ARCH
     export CPU_TYPE=arm920
     #export FIXED_ARCH_TYPE=arm
-
     export KERNEL_DIR=/home/software/Linux/linux-2.6.21-arm1
-    export DFB_DIR=/home/software/Linux/dfb
     export CROSS_COMPILE=arm-none-linux-gnueabi-
     export TOOLCHAIN=/home/software/Linux/toolchain
     export LIB_DIR=$TOOLCHAIN/arm-none-linux-gnueabi/libc/usr/lib
-;;
-
-arm-fsl)
-    ARCH=arm
-    export ARCH_TYPE=$ARCH
-    export CPU_TYPE=cortex-a9
-    export CPU_ARCH=armv7-a
-    #export FIXED_ARCH_TYPE=arm
-
-    export KERNEL_DIR=/home/software/Linux/freescale/linux-3.0.35-c27cb385-20130116
-    export CROSS_COMPILE=arm-fsl-linux-gnueabi-
-    export TOOLCHAIN=/home/software/Linux/freescale/gcc-4.6.2-glibc-2.13-linaro-multilib-2011.12/fsl-linaro-toolchain
-#    export LIB_DIR=$TOOLCHAIN/arm-fsl-linux-gnueabi/libc/usr/lib
-
 ;;
 
 arm-yocto)
@@ -63,7 +46,6 @@ arm-yocto)
     #export PFLAGS="--sysroot=$ROOTFS"
     #export FPU=vfp
     #export FLOAT_ABI=hard
-    BUILD_YOCTO_DRI_BUILD=1
     BUILD_OPTION_USE_OPENCL=1
     BUILD_OPTION_VIVANTE_ENABLE_2D=1
     BUILD_OPTION_MXC_FBDEV=1
@@ -112,28 +94,16 @@ IMX8_Alpha)
 ;;
 
 
-unicore)
-    export ARCH_TYPE=unicore
+X86_PCIE)
+    #export FIXED_ARCH_TYPE=x86_64
+    export KERNEL_DIR=/home/software/Linux/x86_pcie/linux-headers-4.8.0-41-generic
+    export ROOTFS=/
+    export TOOLCHAIN=/usr
+    export CROSS_COMPILE=""
     export CPU_TYPE=0
     export CPU_ARCH=0
-    #export FIXED_ARCH_TYPE=unicore
-
-    export KERNEL_DIR=/home/software/Linux/linux-2.6.32
-    export CROSS_COMPILE=unicore32-linux-
-    export TOOLCHAIN=/home/software/Linux/uc4-1.0-beta-hard-RHELAS5
-    export LIB_DIR=$TOOLCHAIN/unicore32-linux/lib
-;;
-
-tensilica)
-    export ARCH_TYPE=$ARCH
-    export CPU_TYPE=0
-    export CPU_ARCH=0
-    #export FIXED_ARCH_TYPE=tensilica
-
-#    KERNEL_DIR=/home/software/Linux/linux-2.6.24.7
-    CROSS_COMPILE=xtensa_venus-linux-
-    TOOLCHAIN=/home/software/Linux/xtensa/staging_dir/usr
-    LIB_DIR=$TOOLCHAIN/lib
+    export ARCH_TYPE=x86_64
+    export USE_LINUX_PCIE=1
 ;;
 
 ppc-be)
@@ -146,7 +116,7 @@ ppc-be)
     # set ENDIANNESS to build driver with little-endian
     #export ENDIANNESS=-mlittle-endian
 
-#    KERNEL_DIR=/home/software/Linux/linux-2.6.27
+    export KERNEL_DIR=/home/software/Linux/linux-2.6.27
     export CROSS_COMPILE=ppc_4xx-
     export TOOLCHAIN=/home/software/eldk/usr
     export DEPMOD=$TOOLCHAIN/bin/depmod.pl
@@ -155,7 +125,6 @@ ppc-be)
 ;;
 
 mips-le)
-
     export ARCH_TYPE=$ARCH
     export CPU_TYPE=0
     export ARCH_TYPE=mips
@@ -178,7 +147,6 @@ mips-le)
 ;;
 
 mips-be)
-
     export ARCH_TYPE=$ARCH
     export CPU_TYPE=0
     export ARCH_TYPE=mips
@@ -200,24 +168,6 @@ mips-be)
     export LIB_DIR=$TOOLCHAIN/lib
 ;;
 
-mips-le-24kc)
-    export ARCH_TYPE=mips
-    export CPU_ARCH=24kc
-    export CPU_TYPE=0
-
-    #export FIXED_ARCH_TYPE=mips-le-24kc
-
-    #
-    #  set build optons: little-endian
-    #
-    export ENDIANNESS=-mel
-
-    export KERNEL_DIR=/home/software/Linux/linux-2.6.19-mips.le
-    export CROSS_COMPILE=mipsel-linux-gnu-
-    export TOOLCHAIN=/home/software/Linux/tools-2.6.27
-    export LIB_DIR=$TOOLCHAIN/lib
-;;
-
 *)
    echo "ERROR: Unknown $ARCH, or not support so far"
    exit 1
@@ -227,139 +177,20 @@ esac;
 
 
 ########################################################
-# set special build options valule
-# You can modify the build options for different results according your requirement
+# Driver Build Options
 #
-#    option                    value   description                          default value
-#    -------------------------------------------------------------------------------------
-#    DEBUG                      1      Enable debugging.                               0
-#                               0      Disable debugging.
-#
-#    NO_DMA_COHERENT            1      Disable coherent DMA function.                  0
-#                               0      Enable coherent DMA function.
-#
-#                                      Please set this to 1 if you are not sure what
-#                                      it should be.
-#
-#    ABI                        0      Change application binary interface, default    0
-#                                      is 0 which means no setting
-#                                      aapcs-linux For example, build driver for Aspenite board
-#
-#    LINUX_OABI                 1      Enable this if build environment is ARM OABI.   0
-#                               0      Normally disable it for ARM EABI or other machines.
-#
-#    USE_VDK                    1      Eanble this one when the applications           0
-#                                      are using the VDK programming interface.
-#                               0      Disable this one when the applications
-#                                      are NOT using the VDK programming interface.
-#
-#                                      Don't eanble gcdSTATIC_LINK (see below)
-#                                      at the same time since VDK will load some
-#                                      libraries dynamically.
-#
-#    EGL_API_FB                 1      Use the FBDEV as the GUI system for the EGL.    0
-#                               0      Use X11 system as the GUI system for the EGL.
-#
-#    EGL_API_DRI                1      Use DRI to support X accelerator.               0
-#                                      EGL_API_FB and EGL_API_DFB must be 0.
-#                               0      Do not use DRI to support X accelerator.
-#
-#    X11_DRI3                   1      Use DRI3 framework to support X11.              0
-#                               0      Do not use DRI3 framework to support X11.
-#
-#    EGL_API_DFB                1      Use directFB accelerator.                       0
-#                                      EGL_API_FB and EGL_API_DRI must be 0.
-#                               0      Do not use directFB accelerator.
-#
-#    gcdSTATIC_LINK             1      Enable static linking.                          0
-#                               0      Disable static linking;
-#
-#                                      Don't enable this one when you are building
-#                                      GFX driver and HAL unit tests since both of
-#                                      them need dynamic linking mechanisim.
-#                                      And it must NOT be enabled when USE_VDK=1.
-#
-#    CUSTOM_PIXMAP              1      Use the user defined Pixmap format in EGL.      0
-#                               0      Use X11 pixmap format in EGL.
-#
-#    ENABLE_GPU_CLOCK_BY_DRIVER 1      Set the GPU clock in the driver.                0
-#                               0      The GPU clock should be set by BSP.
-#
-#    USE_FB_DOUBLE_BUFFER       0      Use single buffer for the FBDEV                 0
-#                               1      Use double buffer for the FBDEV (NOTE: the FBDEV must support it)
-#
-#
-#    USE_PLATFORM_DRIVER        1      Use platform driver model to build kernel       1
-#                                      module on linux while kernel version is 2.6.
-#                               0      Use legecy kernel driver model.
-#
-#    FPGA_BUILD                 1      To fix a pecical issue on FPGA board;           0
-#                               0      build driver for real chip;
-#
-#
-#    VIVANTE_ENABLE_3D          1      Enable driver support for 3D core;              1
-#                               0      Disable driver support for 3D core;
-#
-#    VIVANTE_ENABLE_2D          1      Enable driver support for 2D core;              1
-#                               0      Disable driver support for 2D core;
-#
-#    VIVANTE_ENABLE_VG          1      Enable driver support for 2D VG core;           0
-#                                      galcore.ko, libGAL.so, and libEGL.so have
-#                                      2D VG support; and build 2D VG driver only;
-#                                      don't build 3D VG driver.
-#                               0      Excluded 2D VG HAL/EGL support;
-#                                      build 3D VG driver only if VIVANTE_ENABLE_3D=1;
-#                                      Don't build 2D VG driver.
-#
-#    GC355_MEM_PRINT            1      Enable memory footprint function in 2D VG driver 0
-#                                      Only work under 2D VG only driver;
-#                               0      Disable memory footprint function in 2D VG driver;
-#
-#    GC355_PROFILER             1      Enable 2D VG driver profiler function;            0
-#                                      Only work under 2D VG driver;
-#                               0      Disable 2D VG driver profiler function;
-#
-#    USE_BANK_ALIGNMENT         1      Enable gcdENABLE_BANK_ALIGNMENT, and video memory 0
-#                                      is allocated bank aligned. The vendor can modify
-#                                      _GetSurfaceBankAlignment() and gcoSURF_GetBankOffsetBytes()
-#                                      to define how different types of allocations are
-#                                       bank and channel aligned.
-#                               0      Disabled (default), no bank alignment is done.
-#
-#    BANK_BIT_START             0      Use default start bit of the bank defined in       0
-#                                      gc_hal_options.h
-#                    [BANK_BIT_START]  Specifies the start bit of the bank (inclusive).
-#                                      This option has an effect only when gcdENABLE_BANK_ALIGNMENT is enabled;
-#
-#    BANK_BIT_END               0      Use default end bit of the bank defined in         0
-#                                      gc_hal_options.h
-#                    [BANK_BIT_END]    Specifies the end bit of the bank (inclusive);
-#                                      This option has an effect only when
-#                                      gcdENABLE_BANK_ALIGNMENT is enabled;
-#
-#    BANK_CHANNEL_BIT           0      Use default channel bit defined in gc_hal_options.h 0
-#                  [BANK_CHANNEL_BIT]  When set to no-zero, video memory when allocated
-#                                      bank aligned is allocated such that render and
-#                                      depth buffer addresses alternate on the channel
-#                                      bit specified. This option has an effect only when
-#                                      gcdENABLE_BANK_ALIGNMENT is enabled.
-#
-#    DIRECTFB_MAJOR_VERSION     1
-#    DIRECTFB_MINOR_VERSION     7
-#    DIRECTFB_MICRO_VERSION     4      DirectFB version supported by GFX driver.
-#                                     Currentlly we support DirectFB-1.7.4.
-#
-#   USE_POWER_MANAGEMENT        1      Enable GPU power managment code;                  1
-#                               0      Disable GPU power management code; Should set
-#                                      it to 0 on FPGA board;
 
 BUILD_OPTION_DEBUG=0
 BUILD_OPTION_ABI=0
 BUILD_OPTION_LINUX_OABI=0
 BUILD_OPTION_NO_DMA_COHERENT=0
 BUILD_OPTION_USE_VDK=1
-BUILD_OPTION_GC355_MEM_PRINT=0
-BUILD_OPTION_GC355_PROFILER=0
+BUILD_OPTION_gcdSTATIC_LINK=0
+BUILD_OPTION_CUSTOM_PIXMAP=0
+BUILD_OPTION_USE_3D_VG=1
+BUILD_OPTION_USE_FB_DOUBLE_BUFFER=0
+BUILD_OPTION_USE_PLATFORM_DRIVER=1
+BUILD_OPTION_FPGA_BUILD=0
 if [ -z $BUILD_OPTION_MXC_FBDEV ]; then
     BUILD_OPTION_MXC_FBDEV=0
 fi
@@ -377,6 +208,12 @@ if [ -z $BUILD_OPTION_X11_DRI3 ]; then
 fi
 if [ -z $BUILD_OPTION_EGL_API_WL ]; then
     BUILD_OPTION_EGL_API_WL=0
+fi
+if [ -z $BUILD_OPTION_EGL_API_X ]; then
+    BUILD_OPTION_EGL_API_X=0
+fi
+if [ -z $BUILD_OPTION_EGL_API_GBM ]; then
+    BUILD_OPTION_EGL_API_GBM=0
 fi
 if [ -z $BUILD_OPTION_EGL_API_NULLWS ]; then
     BUILD_OPTION_EGL_API_NULLWS=0
@@ -396,8 +233,6 @@ if [ -z $BUILD_OPTION_X11_DRI3 ];then
     BUILD_OPTION_X11_DRI3=0
 fi
 
-BUILD_OPTION_gcdSTATIC_LINK=0
-BUILD_OPTION_CUSTOM_PIXMAP=0
 if [ -z $BUILD_OPTION_USE_OPENCL ]; then
     BUILD_OPTION_USE_OPENCL=0
 fi
@@ -407,14 +242,29 @@ fi
 if [ -z $BUILD_OPTION_USE_VULKAN ]; then
     BUILD_OPTION_USE_VULKAN=0
 fi
-BUILD_OPTION_USE_FB_DOUBLE_BUFFER=0
-BUILD_OPTION_USE_PLATFORM_DRIVER=1
 if [ -z $BUILD_OPTION_ENABLE_GPU_CLOCK_BY_DRIVER ]; then
     BUILD_OPTION_ENABLE_GPU_CLOCK_BY_DRIVER=0
 fi
-
-if [ -z $BUILD_YOCTO_DRI_BUILD ]; then
-    BUILD_YOCTO_DRI_BUILD=0
+if [ -z $BUILD_GL4_DRI_BUILD ]; then
+    BUILD_GL4_DRI_BUILD=0
+fi
+if [ -z $BUILD_OPTION_VIVANTE_NO_GL4 ]; then
+    BUILD_OPTION_VIVANTE_NO_GL4=0
+fi
+if [ -z $BUILD_OPTION_VIVANTE_NO_VG ]; then
+    BUILD_OPTION_VIVANTE_NO_VG=0
+fi
+if [ -z $BUILD_OPTION_VIVANTE_ENABLE_DRM ]; then
+    BUILD_OPTION_VIVANTE_ENABLE_DRM=0
+fi
+if [ -z $BUILD_OPTION_VIVANTE_ENABLE_VG ]; then
+    BUILD_OPTION_VIVANTE_ENABLE_VG=0
+fi
+if [ -z $BUILD_OPTION_VIVANTE_ENABLE_3D ]; then
+    BUILD_OPTION_VIVANTE_ENABLE_3D=1
+fi
+if [ -z $BUILD_OPTION_VIVANTE_ENABLE_2D ]; then
+    BUILD_OPTION_VIVANTE_ENABLE_2D=0
 fi
 if [ -z $BUILD_OPTION_MAXCPUS ]; then
     BUILD_OPTION_MAXCPUS=4
@@ -428,24 +278,16 @@ if [ -z $USE_KMS ]; then
     USE_KMS=0
 fi
 
-BUILD_OPTION_CONFIG_DOVEXC5_BOARD=0
-BUILD_OPTION_FPGA_BUILD=0
-BUILD_OPTION_VIVANTE_ENABLE_3D=1
-BUILD_OPTION_VIVANTE_ENABLE_2D=1
-BUILD_OPTION_DIRECTFB_MAJOR_VERSION=1
-BUILD_OPTION_DIRECTFB_MINOR_VERSION=7
-BUILD_OPTION_DIRECTFB_MICRO_VERSION=4
-
 BUILD_OPTIONS="NO_DMA_COHERENT=$BUILD_OPTION_NO_DMA_COHERENT"
 BUILD_OPTIONS="$BUILD_OPTIONS USE_VDK=$BUILD_OPTION_USE_VDK"
-BUILD_OPTIONS="$BUILD_OPTIONS GC355_MEM_PRINT=$BUILD_OPTION_GC355_MEM_PRINT"
-BUILD_OPTIONS="$BUILD_OPTIONS GC355_PROFILER=$BUILD_OPTION_GC355_PROFILER"
+BUILD_OPTIONS="$BUILD_OPTIONS EGL_API_WL=$BUILD_OPTION_EGL_API_WL"
 BUILD_OPTIONS="$BUILD_OPTIONS MXC_FBDEV=$BUILD_OPTION_MXC_FBDEV"
 BUILD_OPTIONS="$BUILD_OPTIONS EGL_API_FB=$BUILD_OPTION_EGL_API_FB"
 BUILD_OPTIONS="$BUILD_OPTIONS EGL_API_DFB=$BUILD_OPTION_EGL_API_DFB"
 BUILD_OPTIONS="$BUILD_OPTIONS EGL_API_DRI=$BUILD_OPTION_EGL_API_DRI"
+BUILD_OPTIONS="$BUILD_OPTIONS EGL_API_X=$BUILD_OPTION_EGL_API_X"
+BUILD_OPTIONS="$BUILD_OPTIONS EGL_API_GBM=$BUILD_OPTION_EGL_API_GBM"
 BUILD_OPTIONS="$BUILD_OPTIONS X11_DRI3=$BUILD_OPTION_X11_DRI3"
-BUILD_OPTIONS="$BUILD_OPTIONS EGL_API_WL=$BUILD_OPTION_EGL_API_WL"
 BUILD_OPTIONS="$BUILD_OPTIONS EGL_API_GBM=$BUILD_OPTION_EGL_API_GBM"
 BUILD_OPTIONS="$BUILD_OPTIONS EGL_API_NULLWS=$BUILD_OPTION_EGL_API_NULLWS"
 BUILD_OPTIONS="$BUILD_OPTIONS gcdSTATIC_LINK=$BUILD_OPTION_gcdSTATIC_LINK"
@@ -453,21 +295,21 @@ BUILD_OPTIONS="$BUILD_OPTIONS ABI=$BUILD_OPTION_ABI"
 BUILD_OPTIONS="$BUILD_OPTIONS LINUX_OABI=$BUILD_OPTION_LINUX_OABI"
 BUILD_OPTIONS="$BUILD_OPTIONS DEBUG=$BUILD_OPTION_DEBUG"
 BUILD_OPTIONS="$BUILD_OPTIONS CUSTOM_PIXMAP=$BUILD_OPTION_CUSTOM_PIXMAP"
+BUILD_OPTIONS="$BUILD_OPTIONS USE_3D_VG=$BUILD_OPTION_USE_3D_VG"
 BUILD_OPTIONS="$BUILD_OPTIONS USE_OPENCL=$BUILD_OPTION_USE_OPENCL"
 BUILD_OPTIONS="$BUILD_OPTIONS USE_OPENVX=$BUILD_OPTION_USE_OPENVX"
 BUILD_OPTIONS="$BUILD_OPTIONS USE_VULKAN=$BUILD_OPTION_USE_VULKAN"
 BUILD_OPTIONS="$BUILD_OPTIONS USE_FB_DOUBLE_BUFFER=$BUILD_OPTION_USE_FB_DOUBLE_BUFFER"
 BUILD_OPTIONS="$BUILD_OPTIONS USE_PLATFORM_DRIVER=$BUILD_OPTION_USE_PLATFORM_DRIVER"
 BUILD_OPTIONS="$BUILD_OPTIONS ENABLE_GPU_CLOCK_BY_DRIVER=$BUILD_OPTION_ENABLE_GPU_CLOCK_BY_DRIVER"
-BUILD_OPTIONS="$BUILD_OPTIONS CONFIG_DOVEXC5_BOARD=$BUILD_OPTION_CONFIG_DOVEXC5_BOARD"
 BUILD_OPTIONS="$BUILD_OPTIONS FPGA_BUILD=$BUILD_OPTION_FPGA_BUILD"
+BUILD_OPTIONS="$BUILD_OPTIONS GL4_DRI_BUILD=$BUILD_GL4_DRI_BUILD"
+BUILD_OPTIONS="$BUILD_OPTIONS VIVANTE_NO_GL4=$BUILD_OPTION_VIVANTE_NO_GL4"
+BUILD_OPTIONS="$BUILD_OPTIONS VIVANTE_NO_VG=$BUILD_OPTION_VIVANTE_NO_VG"
+BUILD_OPTIONS="$BUILD_OPTIONS VIVANTE_ENABLE_DRM=$BUILD_OPTION_VIVANTE_ENABLE_DRM"
 BUILD_OPTIONS="$BUILD_OPTIONS VIVANTE_ENABLE_VG=$BUILD_OPTION_VIVANTE_ENABLE_VG"
 BUILD_OPTIONS="$BUILD_OPTIONS VIVANTE_ENABLE_3D=$BUILD_OPTION_VIVANTE_ENABLE_3D"
 BUILD_OPTIONS="$BUILD_OPTIONS VIVANTE_ENABLE_2D=$BUILD_OPTION_VIVANTE_ENABLE_2D"
-BUILD_OPTIONS="$BUILD_OPTIONS DIRECTFB_MAJOR_VERSION=$BUILD_OPTION_DIRECTFB_MAJOR_VERSION"
-BUILD_OPTIONS="$BUILD_OPTIONS DIRECTFB_MINOR_VERSION=$BUILD_OPTION_DIRECTFB_MINOR_VERSION"
-BUILD_OPTIONS="$BUILD_OPTIONS DIRECTFB_MICRO_VERSION=$BUILD_OPTION_DIRECTFB_MICRO_VERSION"
-BUILD_OPTIONS="$BUILD_OPTIONS YOCTO_DRI_BUILD=$BUILD_YOCTO_DRI_BUILD"
 BUILD_OPTIONS="$BUILD_OPTIONS X11_DRI3=$BUILD_OPTION_X11_DRI3"
 BUILD_OPTIONS="$BUILD_OPTIONS VIVANTE_ENABLE_DRM=$BUILD_OPTION_VIVANTE_ENABLE_DRM"
 BUILD_OPTIONS="$BUILD_OPTIONS -j$BUILD_OPTION_MAXCPUS"
