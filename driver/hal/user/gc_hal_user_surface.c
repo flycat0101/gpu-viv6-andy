3680c8785fe2926561a68551649d426b9a868046
Xiaojun.Chen <xiaojun.chen@verisilicon.com>
2020-07-03 12:17:57 +0800

    CL279029 CAPTURE ONLY MODE: no need to verify dump, since the comamds are skipped and not executed by HW GPU.

--- driver/hal/user/gc_hal_user_surface.c
+++ driver/hal/user/gc_hal_user_surface.c
@@ -12970,7 +12970,7 @@ gcoSURF_BlitCPU(
     }
 
 #if gcdDUMP
-
+#if !gcdCAPTURE_ONLY_MODE
     if (gcvSURF_BITMAP != srcSurf->type)
     {
         gcmDUMP(gcvNULL, "#[info: verify BlitCPU source]");
@@ -12983,6 +12983,7 @@ gcoSURF_BlitCPU(
                        srcSurf->size);
 
     }
+#endif
     /* upload the destination */
     gcmDUMP_BUFFER(gcvNULL,
                    gcvDUMP_BUFFER_MEMORY,
