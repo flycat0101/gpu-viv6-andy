2a7eecaf2f0ce67cdda97940fb9799498ba6c25d
Xiaojun.Chen <xiaojun.chen@verisilicon.com>
2020-07-14 14:23:48 +0800

    Update Copyright

--- driver/driver/khronos/libCL/icdloader12/build_using_cmake.bat
+++ driver/driver/khronos/libCL/icdloader12/build_using_cmake.bat
@@ -1,6 +1,6 @@
 @REM #########################################################################
 @REM #
-@REM #  Copyright (c) 2005 - 2019 by Vivante Corp.  All rights reserved.
+@REM #  Copyright (c) 2005 - 2020 by Vivante Corp.  All rights reserved.
 @REM #
 @REM #  The material in this file is confidential and contains trade secrets
 @REM #  of Vivante Corporation. This is proprietary information owned by
